89c14f59d77d8fb595c332cb31f4875b82a8cab2
NianchurAngelina <kn1b21.nianchur@kpnu.edu.ua>
2023-03-07 16:11:51 +0200

    Lab03

--- Lab03/Lab03_01/Program.cs
+++ Lab03/Lab03_01/Program.cs
@@ -1,40 +1,59 @@
-﻿namespace DerivativeClass
+﻿using System.IO;
+using System.Text;
+using System;
+
+class Person
 {
-    class Building
-    { // базовий клас - будинок
-        public int floors; // кількість поверхів
-        public double area; // площа
-    }
-    class OfficeBuilding : Building
-    { // похідний клас - офісний будинок
-        public int numOffices; // кількість офісів
-    }
-    class LivingBuilding : Building
-    { // ще один похідний клас - житловий будинок
-        public int numFlats; // кількість квартир
-        public int numPeople; // кількість жителів
-    }
-    class Program
+    public void Greet()
     {
-        static void Main()
-        {
-            Building b = new Building(); // створюємо будинок
-            b.area = 1000; // визначаємо площу
-            b.floors = 10; // визначаємо кількість поверхів
-                           // створюємо офісний будинок
-            OfficeBuilding ob = new OfficeBuilding();
-            ob.area = 500; // визначаємо площу
-            ob.floors = 3; // визначаємо кількість поверхів
-            ob.numOffices = 8; // визначаємо кількість офісів
-                               // створюємо житловий будинок
-            LivingBuilding lb = new LivingBuilding();
-            lb.area = 2000; // визначаємо площу
-            lb.floors = 5; // визначаємо кількість поверхів
-            lb.numFlats = 20; // визначаємо кількість квартир
-            lb.numPeople = 75; // визначаємо кількість мешканців
-            Console.WriteLine("Будинок : поверхiв {0} площа {1}", b.floors, b.area);
-            Console.WriteLine("Офiсний будинок : поверхiв {0} площа {1} офiсiв {2}", ob.floors, ob.area, ob.numOffices);
-            Console.WriteLine("Житловий будинок : поверхiв {0} площа {1} квартир {2} мешканцiв {3}", lb.floors, lb.area, lb.numFlats, lb.numPeople);
-        }
+        Console.WriteLine("Hello!");
+    }
+
+    public void SetAge(int age)
+    {
+        this.Age = age;
+    }
+
+    public int Age { get; private set; }
+}
+
+class Student : Person
+{
+    public void Study()
+    {
+        Console.WriteLine("I'm studying");
+    }
+
+    public void ShowAge()
+    {
+        Console.WriteLine("My age is: {0} years old", this.Age);
     }
 }
+
+class Professor : Person
+{
+    public void Explain()
+    {
+        Console.WriteLine("I'm explaining");
+    }
+}
+
+class StudentProfessorTest
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.Greet();
+
+        Student student = new Student();
+        student.SetAge(20);
+        student.Greet();
+        student.ShowAge();
+
+        Professor professor = new Professor();
+        professor.SetAge(40);
+        professor.Greet();
+        professor.Explain();
+    }
+}
+
