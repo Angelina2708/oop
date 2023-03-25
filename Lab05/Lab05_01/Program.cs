d5df685a871f0b2c5957a768deda5fc3695c6a40
NianchurAngelina <angelinanianchur@gmail.com>
2023-03-25 23:52:44 +0200

    Lab05

--- Lab05/Lab05_01/Program.cs
+++ Lab05/Lab05_01/Program.cs
@@ -1,2 +1,92 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Опис інтерфейсу IFigure
+interface IFigure
+{
+    void PrintType();
+    void PrintArea();
+    double Length { get; }
+    double Width { get; }
+    void PrintDiagonal();
+}
+
+// Опис інтерфейсу IColoredFigure
+interface IColoredFigure : IFigure
+{
+    string Color { get; }
+    void PrintColor();
+}
+
+// Реалізація класу Rectangle, який імплементує інтерфейс IFigure
+class Rectangle : IFigure
+{
+    public double Length { get; set; }
+    public double Width { get; set; }
+
+    public Rectangle(double length, double width)
+    {
+        Length = length;
+        Width = width;
+    }
+
+    public void PrintType()
+    {
+        Console.WriteLine("Type: Rectangle");
+    }
+
+    public void PrintArea()
+    {
+        Console.WriteLine($"Area: {Length * Width}");
+    }
+
+    public void PrintDiagonal()
+    {
+        Console.WriteLine($"Diagonal: {Math.Sqrt(Length * Length + Width * Width)}");
+    }
+}
+
+// Реалізація класу ColoredRectangle, який успадковує від класу Rectangle та імплементує інтерфейс IColoredFigure
+class ColoredRectangle : Rectangle, IColoredFigure
+{
+    public string Color { get; set; }
+
+    public ColoredRectangle(double length, double width, string color) : base(length, width)
+    {
+        Color = color;
+    }
+
+    public void PrintColor()
+    {
+        Console.WriteLine($"Color: {Color}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Створюємо екземпляри класів Rectangle та ColoredRectangle
+        Rectangle rect1 = new Rectangle(10, 5);
+        Rectangle rect2 = new Rectangle(7, 3);
+        Rectangle rect3 = new Rectangle(12, 6);
+
+        ColoredRectangle colRect1 = new ColoredRectangle(8, 4, "red");
+        ColoredRectangle colRect2 = new ColoredRectangle(5, 2, "blue");
+        ColoredRectangle colRect3 = new ColoredRectangle(9, 3, "green");
+
+        // Формуємо масив з екземплярів класів
+        IFigure[] figures = new IFigure[] { rect1, rect2, rect3, colRect1, colRect2, colRect3 };
+
+        // Вибираємо лише ті елементи, що підтримують колір та виводимо інформацію про них
+        foreach (IFigure figure in figures)
+        {
+            if (figure is IColoredFigure)
+            {
+                IColoredFigure colFigure = (IColoredFigure)figure;
+                Console.WriteLine("----------------------------");
+                colFigure.PrintType();
+                colFigure.PrintArea();
+                colFigure.PrintDiagonal();
+                colFigure.PrintColor();
+            }
+        }
+    }
+}
--- Lab05/Lab05_03/Program.cs
+++ Lab05/Lab05_03/Program.cs
@@ -1,2 +1,121 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+interface IMaleClothing
+{
+    void DressMale();
+}
+
+interface IFemaleClothing
+{
+    void DressFemale();
+}
+
+abstract class Clothing
+{
+    public string Size { get; set; }
+    public double Cost { get; set; }
+    public string Color { get; set; }
+}
+
+class TShirt : Clothing, IMaleClothing, IFemaleClothing
+{
+    public void DressMale()
+    {
+        Console.WriteLine("Dressing male in a t-shirt of size {0}, color {1}, and cost {2}", Size, Color, Cost);
+    }
+
+    public void DressFemale()
+    {
+        Console.WriteLine("Dressing female in a t-shirt of size {0}, color {1}, and cost {2}", Size, Color, Cost);
+    }
+}
+
+class Pants : Clothing, IMaleClothing, IFemaleClothing
+{
+    public void DressMale()
+    {
+        Console.WriteLine("Dressing male in pants of size {0}, color {1}, and cost {2}", Size, Color, Cost);
+    }
+
+    public void DressFemale()
+    {
+        Console.WriteLine("Dressing female in pants of size {0}, color {1}, and cost {2}", Size, Color, Cost);
+    }
+}
+
+class Skirt : Clothing, IFemaleClothing
+{
+    public void DressFemale()
+    {
+        Console.WriteLine("Dressing female in a skirt of size {0}, color {1}, and cost {2}", Size, Color, Cost);
+    }
+}
+
+class Tie : Clothing, IMaleClothing
+{
+    public void DressMale()
+    {
+        Console.WriteLine("Dressing male in a tie of size {0}, color {1}, and cost {2}", Size, Color, Cost);
+    }
+}
+
+class Shop
+{
+    private string[] sizes = { "XXS", "XS", "S", "M", "L" };
+
+    public string GetDescription(string size)
+    {
+        if (size == "XXS")
+            return "Child size";
+        else
+            return "Adult size";
+    }
+
+    public int GetEuroSize(string size)
+    {
+        return Array.IndexOf(sizes, size) + 32;
+    }
+}
+
+class Atelier
+{
+    public void DressMale(IMaleClothing[] clothes)
+    {
+        foreach (var item in clothes)
+        {
+            item.DressMale();
+        }
+    }
+
+    public void DressFemale(IFemaleClothing[] clothes)
+    {
+        foreach (var item in clothes)
+        {
+            item.DressFemale();
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Clothing[] clothes = new Clothing[]
+        {
+            new TShirt { Size = "M", Color = "Red", Cost = 10.99 },
+            new Pants { Size = "L", Color = "Blue", Cost = 29.99 },
+            new Skirt { Size = "S", Color = "Black", Cost = 24.99 },
+            new Tie { Size = "XS", Color = "Green", Cost = 14.99 }
+        };
+
+        Shop shop = new Shop();
+        Atelier atelier = new Atelier();
+
+        Console.WriteLine("Male clothing:");
+        IMaleClothing[] maleClothing = Array.FindAll(clothes, x => shop.GetDescription(x.Size) == "Adult size").OfType<IMaleClothing>().ToArray();
+        atelier.DressMale(maleClothing);
+
+        Console.WriteLine("\nFemale clothing:");
+        IFemaleClothing[] femaleClothing = Array.FindAll(clothes, x => shop.GetDescription);
+        }
+}
