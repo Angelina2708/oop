ae79871b23353ca9aa7767177d8ec8ee30d01658
NianchurAngelina <angelinanianchur@gmail.com>
2023-04-16 17:01:45 +0300

    Lab09

---
+++ Lab09/Lab09_01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+delegate double Function(double x);
+
+class RectangleIntegral
+{
+    public static double Calculate(double a, double b, int n, Function f)
+    {
+        double h = (b - a) / n;
+        double result = 0.0;
+
+        for (int i = 0; i < n; i++)
+        {
+            double x = a + i * h;
+            result += f(x + h) * h;
+        }
+
+        return result;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Function f = Math.Sin; // функція, яку ми хочемо інтегрувати
+        double a = 0.0;
+        double b = Math.PI;
+        int n = 100; // кількість прямокутників
+
+        double result = RectangleIntegral.Calculate(a, b, n, f);
+
+        Console.WriteLine($"The integral of sin(x) from {a} to {b} is approximately {result}.");
+    }
+}
