3f181c4432d1953f1a416f84c9d449f2fc75de3f
NianchurAngelina <angelinanianchur@gmail.com>
2023-04-16 23:54:35 +0300

    Lab10

--- Lab10/Lab10_02/Program.cs
+++ Lab10/Lab10_02/Program.cs
@@ -1,2 +1,105 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace OneWayListChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OneWayList list1 = new OneWayList();
+            list1 = 'a' + list1;
+            list1 = 'b' + list1;
+            list1 = 'c' + list1;
+            Console.WriteLine("List 1:");
+            list1.Print();
+
+            OneWayList list2 = new OneWayList();
+            list2 = 'c' + list2;
+            list2 = 'b' + list2;
+            list2 = 'a' + list2;
+            Console.WriteLine("List 2:");
+            list2.Print();
+
+            if (list1 == list2)
+                Console.WriteLine("List 1 and List 2 are equal.");
+            else
+                Console.WriteLine("List 1 and List 2 are not equal.");
+
+            list1 = --list1;
+            Console.WriteLine("List 1 after removing first element:");
+            list1.Print();
+
+            Console.ReadLine();
+        }
+    }
+
+    class Node
+    {
+        public char Value;
+        public Node Next;
+    }
+
+    class OneWayList
+    {
+        private Node _head;
+
+        public static OneWayList operator +(char value, OneWayList list)
+        {
+            Node newNode = new Node { Value = value, Next = list._head };
+            list._head = newNode;
+            return list;
+        }
+
+        public static OneWayList operator --(OneWayList list)
+        {
+            if (list._head != null)
+            {
+                list._head = list._head.Next;
+            }
+            return list;
+        }
+
+        public static bool operator ==(OneWayList list1, OneWayList list2)
+        {
+            Node current1 = list1._head;
+            Node current2 = list2._head;
+
+            while (current1 != null && current2 != null)
+            {
+                if (current1.Value != current2.Value)
+                    return false;
+
+                current1 = current1.Next;
+                current2 = current2.Next;
+            }
+
+            return current1 == null && current2 == null;
+        }
+
+        public static bool operator !=(OneWayList list1, OneWayList list2)
+        {
+            return !(list1 == list2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is OneWayList list && this == list;
+        }
+
+        public override int GetHashCode()
+        {
+            return (_head != null ? _head.GetHashCode() : 0);
+        }
+
+        public void Print()
+        {
+            Node current = _head;
+            while (current != null)
+            {
+                Console.Write(current.Value + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Lab10/Lab10_03/Program.cs
+++ Lab10/Lab10_03/Program.cs
@@ -1,2 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+namespace VectorOperations
+{
+    public class Vector
+    {
+        private double[] _data;
+
+        public Vector(int size)
+        {
+            _data = new double[size];
+        }
+
+        public double this[int index]
+        {
+            get => _data[index];
+            set => _data[index] = value;
+        }
+
+        public static Vector operator -(Vector a, Vector b)
+        {
+            if (a._data.Length != b._data.Length)
+            {
+                throw new ArgumentException("Вектори мають мати однаковий розмір.");
+            }
+
+            Vector result = new Vector(a._data.Length);
+            for (int i = 0; i < a._data.Length; i++)
+            {
+                result[i] = a[i] - b[i];
+            }
+
+            return result;
+        }
+
+        public static Vector operator -(Vector a, double scalar)
+        {
+            Vector result = new Vector(a._data.Length);
+            for (int i = 0; i < a._data.Length; i++)
+            {
+                result[i] = a[i] - scalar;
+            }
+
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector a = new Vector(3);
+            Vector b = new Vector(3);
+
+            a[0] = 1.0;
+            a[1] = 2.0;
+            a[2] = 3.0;
+
+            b[0] = 4.0;
+            b[1] = 5.0;
+            b[2] = 6.0;
+
+            Vector result1 = a - b;
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            Console.WriteLine($"Результат віднімання векторів: [{result1[0]}, {result1[1]}, {result1[2]}]");
+
+            double scalar = 1.5;
+            Vector result2 = a - scalar;
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            Console.WriteLine($"Результат віднімання скаляра від вектора: [{result2[0]}, {result2[1]}, {result2[2]}]");
+        }
+    }
+}
